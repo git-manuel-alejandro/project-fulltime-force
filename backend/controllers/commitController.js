65b3d3b5b8532ac460a004bdc5db7b650be9a632
manuel roa ojeda <manuel.roaojeda23@gmail.com>
2022-10-26 11:06:08 -0400

    commit controller, return data

--- backend/controllers/commitController.js
+++ backend/controllers/commitController.js
@@ -4,8 +4,13 @@ const commitController = async (req, res) => {
   const url =
     "https://api.github.com/repos/git-manuel-alejandro/project-fulltime-force/commits";
 
-  const { data } = await axios.get(url);
-  res.json(data[0].commit);
+  try {
+    const { data } = await axios.get(url);
+    res.json(data);
+  } catch (error) {
+    const e = new Error("Something was wrong");
+    return res.status(404).json({ msg: e.message, error });
+  }
 };
 
 export { commitController };
